721a34c6ae091c3397d0ba38b4bb6b2cb5c2076a
alex zhang <alexzhangh@gmail.com>
2012-05-07 22:23:36 -0700

    restructure and clean up

--- Program.cs
+++ Program.cs
@@ -6,13 +6,17 @@ using System.IO;
 
 namespace base64
 {
-	class Program
+	/// <summary>
+	/// Base64 converter.
+	/// </summary>
+	/// <remarks>TODO: add comments regarding the bitmasks for decode/encoding</remarks>
+	public class Base64Converter
 	{
 		static char[] table2 = new char[64];
 		static Dictionary<char, byte> reverse = new Dictionary<char, byte>();
 		
 		//populate the dictionary of index->char
-		static Program ()
+		static Base64Converter ()
 		{
 			for (int i = 65; i <= 90; i++) { //A-Z
 				table2[i - 65] = (char)i;
@@ -32,14 +36,29 @@ namespace base64
 
 		static void Main(string[] args)
 		{
-			Base64ToString("YW55IGNhcm5hbCBwbGVhc3VyZQ==");				
-			StringToBase64("any carnal pleasure");
-			FileToBase64("/home/alex/Pictures/Selection_013.png");
+			if (args.Length > 0) {
+				Console.WriteLine(StringToBase64(args[0]));
+			} else {
+				Console.WriteLine("Please enter an argument");
+			}
+		}
+
+		static void tests()
+		{
+			assert(StringToBase64("any carnal pleasure."), "YW55IGNhcm5hbCBwbGVhc3VyZS4=");
+			assert(StringToBase64("any carnal pleasur"), "YW55IGNhcm5hbCBwbGVhc3Vy");
+			assert(Base64ToString("YW55IGNhcm5hbCBwbGVhc3Vy"), "any carnal pleasur");
+			//Console.WriteLine(StringToBase64("what you see is what you get"));
 		}
 
 		static void assert(bool a)
 		{
 			assert(a, "");
+		}
+
+		static void assert(string a, string b)
+		{
+			assert(a == b);
 		}
 
 		static void assert(bool a, string reason)
@@ -49,13 +68,23 @@ namespace base64
 			}
 		}
 
-		static void Base64ToString(string b64)
+		public static string Base64ToString(string b64)
 		{
+			return new string(Encoding.ASCII.GetChars(Base64ToBytes(b64)));
+		}
+
+		public static void Base64ToFile(string b64, string path)
+		{
+			File.WriteAllBytes(path, Base64ToBytes(b64));
+		}
+
+		public static byte[] Base64ToBytes(string b64)
+		{			
 			assert(b64.Length % 4 == 0, "base 64 length incorrect");
 			int count = b64.Split('=').Length - 1;
 			
 			StringBuilder sb = new StringBuilder(b64.Replace("=", "A"));
-			Console.WriteLine("{0} {1} {2}", b64, sb.ToString(), count);
+			//	Console.WriteLine("{0} {1} {2}", b64, sb.ToString(), count);
 			char[] buffer = new char[4];
 			byte[] bbuf = new byte[4];
 			byte[] retbuf = new byte[3];
@@ -71,71 +100,27 @@ namespace base64
 					bytes.RemoveAt(bytes.Count - 1);
 				}
 			}
-			Console.WriteLine(new string(Encoding.ASCII.GetChars(bytes.ToArray())));
+			return bytes.ToArray();
+		}
+		
+		public static string FileToBase64(string file)
+		{
+			return BytesToBase64(File.ReadAllBytes(file));
+		}
+		
+		public static string StringToBase64(string input)
+		{
+			return StringToBase64(input, Encoding.ASCII);
 		}
 
-		static void charToByte(ref char[] i, ref byte[] j)
-		{
-			for (int foo = 0; foo < i.Length; foo++) {
-				j[foo] = reverse[i[foo]];
-			}
+		public static string StringToBase64(string input, Encoding enco)
+		{					
+			return BytesToBase64(enco.GetBytes(input));
 		}
 
-		static void StringToBase64(string input)
+		public static string BytesToBase64(byte[] ba)
 		{
-			
-			StringBuilder enc = new StringBuilder(input);
-			int numtorep = 0;
-			StringBuilder final = new StringBuilder();
-			int remainder = enc.Length % 3;
-			if (remainder > 0) {
-				numtorep = 3 - remainder;
-				for (int i = 0; i < numtorep; i++) {
-					enc.Append((char)0);
-				}
-			}		
-			assert(enc.Length % 3 == 0, "not divisible by 3");
-			
-			/*   if (enc.Length % 3 == 1)
-            {
-            
-                enc.Append((char)0);
-                enc.Append((char)0);
-                numtorep = 2;
-            }
-            else if (enc.Length % 3 == 2)
-            {
-                enc.Append((char)0);
-                numtorep = 1;
-            }*/
-			byte[] encb = Encoding.ASCII.GetBytes(enc.ToString());
-			byte[] bbfe = new byte[3];
-			char[] buffer = new char[4];
-			byte[] bb = new byte[4];
-			for (int i = 0; i < encb.Length / 3; i++) {                
-				Array.Copy(encb, i * 3, bbfe, 0, 3);
-				process(ref bbfe, ref bb);
-				lookup(ref bb, ref buffer);
-
-				final.Append(buffer);
-			}
-			if (numtorep > 0) {
-
-				final.Remove(final.Length - numtorep, numtorep);
-				if (numtorep == 1) {
-					final.Append("=");
-				} else {                  
-					final.Append("==");
-				}
-			}
-			assert(final.Length % 4 == 0, "final string not divisible by 4");
-			Console.WriteLine(final);
-			Console.ReadKey();
-		}
-
-		static void FileToBase64(string file)
-		{
-			byte[] all = File.ReadAllBytes(file);
+			byte[] all = ba;//File.ReadAllBytes(file);
 			List<byte> enc = new List<byte>(all);
 			int numtorep = 0;
 			StringBuilder final = new StringBuilder();
@@ -147,18 +132,6 @@ namespace base64
 				}
 			}
 			assert(enc.Count % 3 == 0, "not divisible by 3");
-			/*  if (enc.Count% 3 == 1)
-            {
-
-                enc.Add(0);
-                enc.Add(0);
-                numtorep = 2;
-            }
-            else if (enc.Count % 3 == 2)
-            {
-                enc.Add(0);
-                numtorep = 1;
-            }*/
 			byte[] encb = enc.ToArray();
 			byte[] bbfe = new byte[3];
 			char[] buffer = new char[4];
@@ -179,39 +152,40 @@ namespace base64
 				}
 			}
 			assert(final.Length % 4 == 0, "final string not divisible by 4");
-			Console.WriteLine(final);
-			Console.ReadKey();
+			return final.ToString();
 		}
-
-		static void lookup(ref byte[] b, ref char[] ret)
+		#region private methods
+		private static void lookup(ref byte[] b, ref char[] ret)
 		{
 			for (int i = 0; i < b.Length; i++) {
 				ret[i] = table2[b[i]];
 			}
 		}
 
-		static void decode(ref byte[] b, ref byte[] ret)
+		private static void decode(ref byte[] b, ref byte[] ret)
 		{
 			assert(b.Length == 4);
-			ret[0] = (byte)((b[0] << 2) | ((b[1] & 48) >> 4));
-			//	Console.WriteLine(first);
+			ret[0] = (byte)((b[0] << 2) | ((b[1] & 0x30) >> 4));	
 			ret[1] = (byte)(((b[1] & 0xF) << 4) | ((b[2] & 0x3C) >> 2));
-			//	Console.WriteLine(second);
 			ret[2] = (byte)(((b[2] & 0x3) << 6) | b[3]);
-			//Console.WriteLine(third);
 		}
 
-		static void process(ref byte[] b, ref byte[] ret)
+		private static void process(ref byte[] b, ref byte[] ret)
 		{
-			if (b.Length != 3) {
-				throw new Exception("Incorrect length.");
-			}
-			
-			ret[0] = (byte)((b[0] & 252) >> 2);
-			ret[1] = (byte)(((b[0] & 3) << 4) | ((b[1] & 240) >> 4));
-			ret[2] = (byte)(((b[1] & 15) << 2) | ((b[2] & 192) >> 6));
-			ret[3] = (byte)(b[2] & 63); 
+			assert(b.Length == 3);			
+			ret[0] = (byte)((b[0] & 0xFC) >> 2); //1111 1100 mask
+			ret[1] = (byte)(((b[0] & 0x3) << 4) | ((b[1] & 0xF0) >> 4));
+			ret[2] = (byte)(((b[1] & 0xF) << 2) | ((b[2] & 0xC0) >> 6));
+			ret[3] = (byte)(b[2] & 0x3F); 
 		}
+
+		private static void charToByte(ref char[] i, ref byte[] j)
+		{
+			for (int foo = 0; foo < i.Length; foo++) {
+				j[foo] = reverse[i[foo]];
+			}
+		}
+		#endregion
 
 	}
 }
